2608d276ac1252970cc3342661b4b6e58e28ad66
Alex Corey <acorey07@gmail.com>
2025-01-18 11:30:03 -0800

    Update settings.py

--- statuspage/settings.py
+++ statuspage/settings.py
@@ -116,11 +116,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'productionfiles'
 
-STATIC_URL = 'static/'
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
